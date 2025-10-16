c0931f28453a127cc621d37d9e9350e04b8622f6
arpan7sarkar <arpansakar@gmail.com>
2025-10-16 19:41:30 +0530

    Understand about passing params inside function

--- 0_start/src/App.jsx
+++ 0_start/src/App.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   const btnClick = () => {
-    console.log('Button Clicked')
-  }
+    console.log("Button Clicked");
+  };
+  const typeChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <div>
       <button onClick={btnClick}>Click</button>
-      <button onClick={() => console.log('Button Clicked hua')}>Click</button>
-      <input onChange={function(e){
-        console.log(e.target.value);
-      }} className='mt-10 px-15 py-7 border-2' type="text" name="" placeholder='Enter your name' id="" />
+      <button onClick={() => console.log("Button Clicked hua")}>Click</button>
+      <input
+        onChange={function (e) {
+          console.log(e.target.value);
+        }}
+        className="mt-10 px-15 py-7 border-2"
+        type="text"
+        name=""
+        placeholder="Enter your name"
+        id=""
+      />
+      <input
+        onChange={function (e) {
+          typeChange(e);
+        }}
+        className="mt-10 px-15 py-7 border-2"
+        type="text"
+        name=""
+        placeholder="Track your typing"
+        id=""
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
