7896e105d93fb5e9f629613d4075e3ee0dd74768
arpan7sarkar <arpansakar@gmail.com>
2025-10-18 21:41:21 +0530

    understanding forms in react

--- 0_start/src/App.jsx
+++ 0_start/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./components/Counter";
 import Pobj from "./components/Pobj";
+import Form from "./components/Form";
 const App = () => {
 
   return (
     <div>
+      <Form/>
       <Pobj/>
       <Counter />
     </div>
---
+++ 0_start/src/components/Form.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const formHandler=(e)=>{
+        e.preventDefault();//this will prevent the default behaviour of form which is to reload the page
+        console.log("Form had been submitted")
+    }
+  return (
+    <div>
+      <form onSubmit={formHandler}>
+        //here by default we are directly trying to implement form that is not a good sign
+        //we should use react to interact with form
+        <input  className="invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20 ..." type="text" name="name" placeholder='Enter Your Name' id="" />
+        <button className="mx-4  p-4 bg-blue-500 rounded-lg ">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
