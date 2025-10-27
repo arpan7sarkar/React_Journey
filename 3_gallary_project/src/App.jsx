2e354a78b372a43dedf813de01e4adec92729295
arpan7sarkar <arpansakar@gmail.com>
2025-10-27 13:18:06 +0530

    initialized gallary project

---
+++ 3_gallary_project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white h-screen'>
+      <div>Hello world</div>
+    </div>
+  )
+}
+
+export default App
