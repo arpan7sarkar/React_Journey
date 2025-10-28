2495ca8456d5a7c1636833ce6e4124ad3a48abea
arpan7sarkar <arpansakar@gmail.com>
2025-10-28 22:05:47 +0530

    half done

--- 3_gallary_project/src/App.jsx
+++ 3_gallary_project/src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const App = () => {
+  const [photoData, setPhotoData] = useState([]);
+  const getData = async () => {
+    const responce = await axios.get("https://picsum.photos/v2/list");
+    setPhotoData(responce.data);
+    console.log(photoData);
+  };
+  let photoDiv = "e ";
+  const len = Object.keys(photoData).length;
+  if (len > 0) {
+    {
+     photoDiv= photoData.map((idx, e) => {
+        return <div key={idx}>photo {e.download_url}</div>;
+      });
+    }
+  }
   return (
-    <div className='bg-black h-screen'>
-      <button className='bg-blue-400 active:scale-95 px-4 py-2 rounded-xl'>Click me</button>
+    <div className="bg-black h-screen text-cyan-200">
+      <button
+        onClick={getData}
+        className="bg-blue-400 active:scale-95 px-4 py-2 rounded-xl"
+      >
+        Click me
+      </button>
+      <div>{photoDiv}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
