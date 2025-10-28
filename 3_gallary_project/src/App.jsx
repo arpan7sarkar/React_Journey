b5bd1ef25066279ab56932e76b8392d8d20e920b
arpan7sarkar <arpansakar@gmail.com>
2025-10-28 23:43:08 +0530

    used useEffect to directly render the images

--- 3_gallary_project/src/App.jsx
+++ 3_gallary_project/src/App.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [photoData, setPhotoData] = useState([]);
   const getData = async () => {
-    const responce = await axios.get("https://picsum.photos/v2/list");
+    const responce = await axios.get(
+      "https://picsum.photos/v2/list?page=3&limit=40"
+    );
     setPhotoData(responce.data);
     console.log(photoData);
   };
@@ -12,23 +14,27 @@ const App = () => {
   const len = Object.keys(photoData).length;
   if (len > 0) {
     {
-      photoDiv = photoData.map(( e) => {
+      photoDiv = photoData.map((e) => {
         return (
-          <div className="rounded-xl">
-            <img src={e.download_url} alt="" className="object-cover w-77 h-70 overflow-auto" />
+          <div>
+            <div className="rounded-xl overflow-hidden">
+              <img
+                src={e.download_url}
+                alt=""
+                className="object-cover w-77 h-70 overflow-auto"
+              />
+              <div>{e.author}</div>
+            </div>
           </div>
         );
       });
     }
   }
+  useEffect(() => {
+    getData();
+  })
   return (
     <div className="bg-black h-screen text-cyan-200 overflow-auto">
-      <button
-        onClick={getData}
-        className="bg-blue-400 active:scale-95 px-4 py-2 rounded-xl"
-      >
-        Click me
-      </button>
       <div className="flex flex-wrap gap-5">{photoDiv}</div>
     </div>
   );
