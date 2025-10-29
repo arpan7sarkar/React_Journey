46e8696a4f58fbd8df771e26000ad8e67f2bb25e
arpan7sarkar <arpansakar@gmail.com>
2025-10-29 11:23:37 +0530

    understand about useNavigate

--- 4_Advanced_routing/src/pages/Notfound.jsx
+++ 4_Advanced_routing/src/pages/Notfound.jsx
@@ -1,8 +1,15 @@
+import { use } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Notfound = () => {
+  let navigate = useNavigate();//use navigate is also comes from react router dom and it is used for navigation/works similer to LINK tag
+  const btnClicked = () => {
+    navigate('/')
+  }
   return (
-    <div className="text-red-600">
+    <div className="text-red-600 ">
       <h2>404 Not found</h2>
+      <button onClick={btnClicked} className="active:scale-95 cursor-pointer bg-red-700 p-5 text-lg text-white font-semibold rounded-3xl absolute bottom-1/3 left-180">Go to Home</button>
     </div>
   )
 }
